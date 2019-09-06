b5a1131ef8c38e8bfb99a877d06fde271200ae54
imorrell <imorrell@ltu.edu>
2019-09-06 18:50:32 -0400

    Add files via upload

---
+++ FunActivities.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunRecommendations
+{
+    class FunActivities
+    {
+        //intialize class variables
+        int userChoice;
+        String activity;
+        String transportation;
+        int myPeeps;
+
+        //intialize constructor that accepts one parameter
+        public FunActivities(int choice)
+        {
+            userChoice = choice;
+        }
+
+        //create a method that determines what activity was selected based off the numeric input
+        public String userActivity()
+        {
+            //use switch statement to determine and identify which activity was selected
+            switch (userChoice)
+            {
+                //If user selects action
+                case 1:
+                {
+                        activity = "action";
+                   break;
+                }
+                
+                //if user selects Chill
+                case 2:
+                {
+                        activity = "chill vibes";
+                        break;
+                }
+
+                //if user select danger
+                case 3:
+                {
+                        activity = "danger";
+                        break;
+                }
+
+                //user selects slapping food
+                case 4:
+                {
+                        activity = "slapping food";
+                        break;
+                }
+
+                default:
+                    break;
+            }
+
+            return activity;
+        }
+
+        //create a String method that accepts an integer parameter for number of friends. Based off the number,
+        //return a string for the method of transportaion that the user should take.
+        public String howWeRide(int people)
+        {
+
+            //use if else statements to determine what vehicle of tranportation to take based off the number of people
+            if (people == 0)
+            {
+                //take your scooter
+                transportation = "laser fast scooter!";
+            }
+            else if (people >=1 && people <= 4)
+            {
+                //Lets take the Benz
+                transportation = " Big Body Benz!";
+            }
+            else if (people >= 5 && people <= 10)
+            {
+                //Lets take the party bus
+                transportation = "dubbed out Party bus!";
+            }
+            else if (people >= 1 && people <= 4)
+            {
+                //Lets take the private jet
+                transportation = "private jet";
+            }
+
+
+            return transportation;
+        }
+
+        //create method that has distinctive messages for what the user is doing, who is going, and transportation
+        public void displayMessage()
+        {
+            //if user is looking for action
+            if (activity == "action")
+            {
+                Console.WriteLine("If you're in the mood for some " + activity + 
+                    " tonight, you should go to CJ Barrymores and travel using a " + transportation);
+            }
+
+            else if (activity == "chill vibes")
+            {
+                Console.WriteLine("If you're in the mood for some " + activity +
+                    " tonight, you should go to The Commons and travel using a " + transportation);
+            }
+
+            else if (activity == "danger")
+            {
+                Console.WriteLine("If you're in the mood for some " + activity +
+                    " tonight, you should go to bunjee jumping and travel using a " + transportation);
+            }
+
+            else
+            {
+                Console.WriteLine("If you're in the mood for some " + activity +
+                    " tonight, you should go to Buckets & Biscuits and travel using a " + transportation);
+            }
+
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿//Programer: Ivoire Morrell
+//Program: Fun Reccomendations
+//Date: September 6, 2019
+//Description: This program uses conditional statements to reccomend ideas for fun to the user
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunRecommendations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Intialize variables
+            int userChoice;
+            bool ok = false;
+            String activity;
+            String transportation;
+            int myPeeps;
+
+            //diplay welcome message
+            Console.WriteLine("What up fam! What you trying to get into tonight?" +
+                " Here are a few options (enter number for corresponding activity)" + "\n");
+
+            //use while loop to ensure user selects one of the available options
+            while (ok == false)
+            {
+                Console.WriteLine("1) Action" + "\n" +
+                              "2) Chill vibes" + "\n" +
+                              "3) Danger" + "\n" +
+                              "4) Slapping Food" + "\n"
+                              );
+
+                //read user input
+                userChoice = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine();
+
+                //Determine if the user choice is one of the above options
+                if (userChoice >= 1 && userChoice <= 4)
+                { 
+                    //user enter one of the above options.
+                    ok = true;
+
+                    //create a fun activities object that sets the different variables
+                    FunActivities active = new FunActivities(userChoice);
+
+                    //ask the user how many people are coming
+                    Console.WriteLine("How many of your peoples are you bringing?" + "\n");
+
+                    //get user entry
+                    myPeeps = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine();
+
+                    //use methods from FunActivities class to return the activity, transportation
+                    activity = active.userActivity();
+
+                    transportation = active.howWeRide(myPeeps);
+
+                    //Display what activity the user selected along with the method of transportation
+                    active.displayMessage();
+                }
+                else
+                {
+                    //wrong entry. User must select one of the correct options.
+                    Console.WriteLine("Whoa I dont know about that actitvty. Please select from options below" + "\n");
+                    ok = false;
+                }
+
+            }
+
+            //Display goodbye message
+            Console.WriteLine();
+
+            Console.WriteLine("Dueces!");
+
+            Console.ReadKey();
+            
+        }
+    }
+}
